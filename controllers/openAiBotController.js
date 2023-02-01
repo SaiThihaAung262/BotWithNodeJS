e994d2d17b37e21aa30dc6e8d3e2c83324c23761
minghua <minghua@gmail.com>
2023-02-01 15:53:30 +0630

    --

--- controllers/openAiBotController.js
+++ controllers/openAiBotController.js
@@ -11,7 +11,6 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 exports.makeQuestionToBot = async (req, res) => {
-  console.log("Here is request", req);
   if (!req.body.question) {
     req.json({
       err_code: 412,
@@ -20,15 +19,15 @@ exports.makeQuestionToBot = async (req, res) => {
   }
   try {
     const completion = await openai.createCompletion({
-      model: "text-davinci-003",
+      model: "text-davinci-002",
       prompt: req.body.question,
       temperature: 0,
       max_tokens: 200,
     });
-
     res.json(
       helper.ResponseData(0, "success", completion.data.choices[0].text)
     );
+    // res.json(helper.ResponseData(0, "success", "Leee"));
   } catch (error) {
     console.log("Here have some error", error);
     res.json(helper.ResponseData(500, "Internal server error!", error));
